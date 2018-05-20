58e16875d797857378540044b127ae295bfdf1ab
MalikaSajjad <36885067+MalikaSajjad@users.noreply.github.com>
2018-05-20 07:33:34 +0000

    detailed info of a given process by using the PID

--- Assignments/BCSF13A552_SP_Assignment-02/p4.py
+++ Assignments/BCSF13A552_SP_Assignment-02/p4.py
@@ -5,15 +5,19 @@ import sys
 import os
 import time
 
-p = psutil.Process(int(sys.argv[1]))
-print "process ID: ", p.pid
-print "process Name: ", p.name
-print "process Status: ", p.status
-print "process Parent ID: ", p.ppid
-print "process Parent Name: ", psutil.Process(p.ppid).name
-print "Process Creation Time:", time.ctime(p.create_time)
-print "Files opened by the process info:", p.get_open_files()
-print "Memory Info (In Bytes):", p.get_memory_info()
+try :
+	p = psutil.Process(int(sys.argv[1]))
+except:
+	print "no sufficient args"
+else:
+	print "process ID: ", p.pid
+	print "process Name: ", p.name
+	print "process Status: ", p.status
+	print "process Parent ID: ", p.ppid
+	print "process Parent Name: ", psutil.Process(p.ppid).name
+	print "Process Creation Time:", time.ctime(p.create_time)
+	print "Files opened by the process info:", p.get_open_files()
+	print "Memory Info (In Bytes):", p.get_memory_info()
 
 
 #if __name__ == __main__:	
